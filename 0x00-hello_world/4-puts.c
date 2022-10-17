72755c9b35d099c350e95dae35230fd4073e5cae
Etiennekoutouan <etiennekoutouan53@gmail.com>
2022-10-16 18:02:07 -0700

    mere

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #!/bin/bash 
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle);
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
 

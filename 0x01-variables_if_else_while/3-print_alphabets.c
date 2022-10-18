e3774e70924f8828f0f36741cb5deb41645bd814
Etiennekoutouan <etiennekoutouan53@gmail.com>
2022-10-18 12:55:21 -0700

    rusty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,23 @@
-#!/bin/bash 
-gcc -S 
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 no error, non zero if error.
+ **/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+ce projet est un projet sur les variable en c

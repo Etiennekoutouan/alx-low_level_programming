86f980690a741b4ceb20205395820386da9ef2f2
Etiennekoutouan <etiennekoutouan53@gmail.com>
2022-11-03 14:58:12 -0700

    hey

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+*_puts_recursion - imprimer une chaîne
+*@s:chaîne
+*Retour:annulé
+*/
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(S +1);
+}
+autre
+{
+	_putchar('\n');
+}
+
+}

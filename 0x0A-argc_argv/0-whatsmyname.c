200211b2dad97c640830750c0209a161e940b890
Etiennekoutouan <etiennekoutouan53@gmail.com>
2022-11-07 14:35:34 -0800

    joel

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - imprime son nom, suivi d’une nouvelle ligne.
+*@argc : nombre d’arguments
+*@argv : arguments
+*Retour: 0
+*/
+int main(int argc, char**argv)
+{
+	(nul) argc;
+	printf("« %s\n »,argv[0]");
+	retour(0);
+}

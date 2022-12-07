b7d65ac1105c2b2adf4dfe7b9caeb6d7672b69bd
Etiennekoutouan <etiennekoutouan53@gmail.com>
2022-12-07 09:56:59 -0800

    finish

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,18 +2,15 @@
 #include <stdio.h>
 /**
 *print_listint - prints all the elements of a listint_t list.
-*
-*
 *@h: head of linklist no
-*
-** Return: the number of nodes
+*Return: the number of nodes
 **/
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;
+	size_t count = 0;
 while (h != NULL)
 {
-printf("%d\n", h->n);
+	printf("%d\n", h->n);
 h = h->nex
 	count++;
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
 /**
@@ -6,14 +6,12 @@ fndef LISTS_H
 *@n: integer
 *@next: points to the next node
 *Description: singly linked list node structure
-*
-**/
+*/
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
-}
-listint_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
